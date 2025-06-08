7ea85128183d215f93b9ee7e209ecf991ff8bdc2
phumiphat <phumiphat.auk@gmail.com>
2025-06-08 11:09:47 +0700

    Implement chat screens for get messages

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/widgets/chat_messages.dart';
+import 'package:flutter_chat_app/widgets/new_messages.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -21,7 +23,12 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(child: Text('Chat Screen')),
+      body: Column(
+        children: [
+          Expanded(child: ChatMessages()),
+          NewMessages(),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/chat_messages.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: false)
+          .snapshots(),
+      builder: (ctx, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (!chatSnapshot.hasData || chatSnapshot.data!.docs.isEmpty) {
+          return const Center(child: Text('No messages yet.'));
+        }
+
+        if (chatSnapshot.hasError) {
+          return const Center(child: Text('Something went wrong...'));
+        }
+
+        final loadedMessages = chatSnapshot.data!.docs;
+
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (ctx, index) {
+            final chatDoc = loadedMessages[index].data()['text'];
+            return Text(chatDoc);
+          },
+        );
+      },
+    );
+  }
+}
